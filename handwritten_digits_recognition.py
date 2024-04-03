6d02375e07f046289d1e1437355a0bb3d0f06026
p0daru <102858022+p0daru@users.noreply.github.com>
2024-04-03 16:55:07 +0300

    Update handwritten_digits_recognition.py

--- handwritten_digits_recognition.py
+++ handwritten_digits_recognition.py
@@ -4,55 +4,55 @@ import numpy as np
 import tensorflow as tf
 import matplotlib.pyplot as plt
 
-# Decide if to load an existing model or to train a new one
+# Тренувати нову модель чи завантажувати існуючу 
 train_new_model = True
 
 if train_new_model:
-    # Loading the MNIST data set with samples and splitting it
+    # Завантаження набору даних MNIST та розділення на тренувальний і тестувальний набори
     mnist = tf.keras.datasets.mnist
     (X_train, y_train), (X_test, y_test) = mnist.load_data()
 
-    # Normalizing the data (making length = 1)
+    # Нормалізація даних (приведення до діапазону від 0 до 1)
     X_train = tf.keras.utils.normalize(X_train, axis=1)
     X_test = tf.keras.utils.normalize(X_test, axis=1)
 
-    # Create a neural network model
-    # Add one flattened input layer for the pixels
-    # Add two dense hidden layers
-    # Add one dense output layer for the 10 digits
+    # Створення моделі нейронної мережі
+    # Вхідний шар для плоского зображення 28x28 пікселів
+    # Два приховані шари з 128 нейронами та функцією активації ReLU
+    # Вихідний шар з 10 нейронами (відповідає 10 цифрам) та функцією активації softmax
     model = tf.keras.models.Sequential()
-    model.add(tf.keras.layers.Flatten(input_shape=(28, 28)))
+    model.add(tf.keras.layers.Flatten(input_shape=(28, 28))) 
     model.add(tf.keras.layers.Dense(units=128, activation="relu"))
     model.add(tf.keras.layers.Dense(units=128, activation="relu"))
     model.add(tf.keras.layers.Dense(units=10, activation="softmax"))
 
 
-    # Compiling and optimizing model
+    # Компіляція та оптимізація моделі
     model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
-    # Training the model
+    # Тренування моделі
     model.fit(X_train, y_train, epochs=5)
 
-    # Evaluating the model
+    # Оцінка моделі
     val_loss, val_acc = model.evaluate(X_test, y_test)
     print(val_loss)
     print(val_acc)
 
-    # Saving the model
+    # Збереження моделі
     model.save('/Users/pogoriladaria/Dropbox/Mac/Desktop/handwritten-digits-recognition/handwritten_digits.keras')
 else:
-    # Load the model (завантажити вже натреновану модель)
+    # Завантажити існуючу модель
     model = tf.keras.models.load_model('/Users/pogoriladaria/Dropbox/Mac/Desktop/handwritten-digits-recognition/handwritten_digits.keras')
 
-# Load custom images and predict them
+# Завантаження кастомних зображень та їх передбачення
 image_number = 1
 while os.path.isfile('digits/digit{}.png'.format(image_number)):
     try:
-        img = cv2.imread('digits/digit{}.png'.format(image_number))[:,:,0]
-        img = np.invert(np.array([img]))
-        prediction = model.predict(img)
-        print("The number is probably a {}".format(np.argmax(prediction))) # which neuron has the highest activation 
-        plt.imshow(img[0], cmap=plt.cm.binary)
+        img = cv2.imread('digits/digit{}.png'.format(image_number))[:,:,0] # зчитування зображення та конвертація у чорно-білий формат
+        img = np.invert(np.array([img])) # інверсія кольорів та створення numpy масиву
+        prediction = model.predict(img) # передбачення
+        print("The number is probably a {}".format(np.argmax(prediction))) # argmax – який з нейронів має найвищу активацію (highest activation)
+        plt.imshow(img[0], cmap=plt.cm.binary) # відображення зображення
         plt.show()
         image_number += 1
     except:
